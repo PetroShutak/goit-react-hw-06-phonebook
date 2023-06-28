c2cb5aea8c355bae5f16608688dd42eff884f201
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-06-28 19:32:28 +0300

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
+
 function App() {
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './components/App';
 import store from './redux/store';
+import App from './components/App';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
