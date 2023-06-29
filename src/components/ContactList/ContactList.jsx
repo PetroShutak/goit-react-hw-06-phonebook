42dae2f8dac9873d7b94c05539b909fd963f80f9
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-06-29 19:37:31 +0300

    fixed li

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,27 @@
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
 import { deleteContact } from 'redux/contactsSlice';
 
 const ContactList = () => {
   const contacts = useSelector(state => state.contacts.contacts);
   const dispatch = useDispatch();
 
+  const handleDelete = id => {
+    dispatch(deleteContact(id));
+  };
+
   return (
-    <ul>
+    <div>
       <h2>Contact List</h2>
-      {contacts.map(({ id, name, number }) => (
-        <li
-          key={id}
-          name={name}
-          id={id}
-          number={number}
-          onClick={() => dispatch(deleteContact(id))}
-        >
-          {name}: {number}
-        </li>
-      ))}
-    </ul>
+      <ul>
+        {contacts.map(({ id, name, number }) => (
+          <li key={id}>
+            {name}: {number}
+            <button onClick={() => handleDelete(id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
